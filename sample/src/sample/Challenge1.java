24e4478b77921d9c2acdd3a045b99d73ca42927b
Anil kumar <devarianil415@gmail.com>
2022-05-26 20:14:16 -0400

    changes in sample

--- sample/src/sample/Challenge1.java
+++ sample/src/sample/Challenge1.java
@@ -3,7 +3,7 @@ package sample;
 public class Challenge1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hi"+"\n"+"Hello"+"\n"+"Bye");
+		System.out.println("Hi"+"\n"+"Hello"+"\n"+"By");
 	}
 
 }
